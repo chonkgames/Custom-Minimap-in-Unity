aa96cd02972b59ea00eb807f506efaf85f962021
chonk <117404021+chonkgames@users.noreply.github.com>
2024-12-15 22:51:06 +0100

    Update MapEntity.cs

--- Scripts/MapEntity.cs
+++ Scripts/MapEntity.cs
@@ -1,1 +1,30 @@
+using UnityEngine;
 
+public class MapEntity : MonoBehaviour
+{
+	#region Variables
+
+	[SerializeField] private MapEntityData data;
+	public MapEntityData Data => data;
+
+	private MapIcon _icon;
+
+	#endregion
+	
+	#region Unity Methods
+	
+	private void Start()
+	{
+		_icon = MapManager.Instance.RegisterMapEntity(this);
+	}
+
+	private void OnDisable()
+	{
+		if (_icon != null)
+		{
+			MapManager.Instance.UnregisterMapEntity(_icon);
+		}
+	}
+	
+	#endregion
+}
